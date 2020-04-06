96aacdd92f05ebdc6d34fd2b0d74b02585e81fc3
Jack Cooper <jackcooper1245@gmail.com>
2020-04-06 17:54:46 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,4 +1,3 @@
-def parrot(phrase)
-  return "Squawk!"
-  end# Create method `parrot` that outputs a given phrase and
+def parrot(phrase = "Squawk!")
+end# Create method `parrot` that outputs a given phrase and
 # returns the phrase

41c4e69db7dbf7d323087da3327e711f91349c2c
Jack Cooper <jackcooper1245@gmail.com>
2020-04-06 17:57:50 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,1 @@
-
-def parrot(phrase)
-  if phrase
-    return phrase
-  else
-  return "Squawk!"
-end
-end# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+filter_parameter_logging
